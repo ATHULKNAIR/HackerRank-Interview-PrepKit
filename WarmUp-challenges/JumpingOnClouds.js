ef1c1e408f4f4bd1823e400183dce59a0dff3d9a
ATHULKNAIR <athul2639@gmail.com>
2021-05-31 22:45:15 +0530

    Jumping Clouds

---
+++ WarmUp-challenges/JumpingOnClouds.js
@@ -0,0 +1,22 @@
+function jumpingOnClouds(c) {
+
+    let jumps = 0;
+    
+    for (let i = 0; i < c.length; i++) {
+        if (c[i] === 0) {
+            if (c[i + 2] === 0) {
+                jumps++;
+                i = i + 1;
+                continue;
+            }
+            if (c[i + 1] === 0 && (c[i + 2] === 1 || !(c[i + 2]))) {
+                jumps++;
+                continue;
+            }
+        } else {
+            continue;
+        }
+    }
+
+    return jumps;
+}
