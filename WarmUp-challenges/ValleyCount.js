ea1bb57e41b12aebd28861d466bdea5daae873fd
ATHULKNAIR <athul2639@gmail.com>
2021-05-31 22:46:08 +0530

    Valley Count

---
+++ WarmUp-challenges/ValleyCount.js
@@ -0,0 +1,21 @@
+function countingValleys(steps, path) {
+    // Write your code here
+
+    let paths = path.split('');
+    let valleyCount = 0;
+    let seaLevel = 0;
+    let valley = false;
+
+    paths.forEach(path => {
+        path === 'U' ? seaLevel++ : seaLevel-- ;
+
+        if (seaLevel < 0 && !valley) {
+            valleyCount++;
+            valley = true;
+        } else if (seaLevel >= 0 && valley) {
+            valley = false
+        }
+    })
+
+    return valleyCount;
+}
