12af8e6cef946f07ed83ccd17ade0343684422a4
ATHULKNAIR <athul2639@gmail.com>
2021-06-04 21:30:48 +0530

    My Stack in jS

---
+++ Misc/myStack.js
@@ -0,0 +1,72 @@
+
+class Stack {
+    constructor(size) {
+        this.items = [];
+        this.size = size;
+    }
+
+    add(element) {
+        if (!this.isFull()) {
+            return this.items.push(element);
+        }
+    }
+
+    remove() {
+        if (!this.isEmpty()) {
+            return this.items.pop();
+        }
+    }
+
+    peek() {
+        if (!this.isEmpty()) {
+            return this.items[this.items.length - 1];
+        }
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    isFull() {
+        return this.items.length === this.size;
+    }
+
+    clear() {
+        this.items = [];
+    }
+    print() {
+        if (!this.isEmpty()) {
+            var top = this.items.length - 1;
+            while (top >= 0) {
+                console.log(this.items[top]);
+                top--;
+            }
+        } else {
+            console.log("No items to print")
+        }
+    }
+}
+
+let stack = new Stack(5);
+stack.add(1);
+stack.add(2);
+stack.add(4);
+stack.add(8);
+
+console.log("Items in Stack : ");
+stack.print()
+
+console.log("Removing item... ");
+stack.remove();
+
+console.log("After removing, Items : ");
+stack.print()
+
+
+console.log("First Item : " + stack.peek());
+
+console.log(stack.isEmpty());
+
+stack.clear();
+stack.print()
+
+
