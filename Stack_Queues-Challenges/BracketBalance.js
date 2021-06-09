cb4c6f04ce57dbba71c48abc247acef2a6a56800
ATHULKNAIR <athul2639@gmail.com>
2021-06-09 22:47:21 +0530

    Check if there is a balance of brackets

---
+++ Stack_Queues-Challenges/BracketBalance.js
@@ -0,0 +1,22 @@
+function isBalanced(s){
+
+    let brackets = "[]{}()<>"
+    let stack = []
+  
+    for(let bracket of s) {
+      let bracketsIndex = brackets.indexOf(bracket)
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(brackets[bracketsIndex + 1])
+      } 
+      else {
+        if(stack.pop() !== brackets[bracketsIndex]) {
+          return false;
+        }
+      }
+    }
+    return stack.length === 0
+   
+  }
+
+  console.log(isBalanced('[()][{}[{}[{}]]][]{}[]{}[]{{}({}(){({{}{}[([[]][[]])()]})({}{{}})})}'))
