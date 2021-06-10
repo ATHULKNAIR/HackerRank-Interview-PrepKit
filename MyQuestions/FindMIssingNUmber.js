6c1dc6c3185f9cbda6681fc00fb13a0890a0f9f5
ATHULKNAIR <athul2639@gmail.com>
2021-06-10 23:26:49 +0530

    FInd the missing number from an unsorted array

---
+++ MyQuestions/FindMIssingNUmber.js
@@ -0,0 +1,25 @@
+const array = [10,5,4,7,2,9,1,3,6]  
+
+// One number is missing  .. find it
+
+
+
+let n = array.length+1;
+let total = n * (n+1)/2;
+
+let arrayTotal = array.reduce((t,i)=> t+i);
+console.log("Missing Number : "+(total-arrayTotal))
+
+////////////////////////////////////////
+
+let arr = array.sort((a,b)=> a-b)
+
+for(let i = 0; i<arr.length;i++){
+    if(arr[i] !== i+1){
+        return console.log("Missing Number : "+(i+1))
+    }
+}
+
+
+
+
