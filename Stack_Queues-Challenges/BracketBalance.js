3393fed3be4593b474ad7f4a31d28d7da3f598ee
ATHULKNAIR <athul2639@gmail.com>
2021-06-15 22:17:58 +0530

    Bracket Balance using stack

--- Stack_Queues-Challenges/BracketBalance.js
+++ Stack_Queues-Challenges/BracketBalance.js
@@ -1,22 +1,26 @@
-function isBalanced(s){
+function isBalanced(s) {
 
     let brackets = "[]{}()<>"
     let stack = []
-  
-    for(let bracket of s) {
-      let bracketsIndex = brackets.indexOf(bracket)
-  
-      if(bracketsIndex % 2 === 0) {
-        stack.push(brackets[bracketsIndex + 1])
-      } 
-      else {
-        if(stack.pop() !== brackets[bracketsIndex]) {
-          return false;
+
+    for (let bracket of s) {
+        let bracketsIndex = brackets.indexOf(bracket)
+
+        if (bracketsIndex === -1) {
+            continue
         }
-      }
+
+        if (bracketsIndex % 2 === 0) {
+            stack.push(brackets[bracketsIndex + 1])
+        }
+        else {
+            if (stack.pop() !== brackets[bracketsIndex]) {
+                return false;
+            }
+        }
     }
     return stack.length === 0
-   
-  }
 
-  console.log(isBalanced('[()][{}[{}[{}]]][]{}[]{}[]{{}({}(){({{}{}[([[]][[]])()]})({}{{}})})}'))
+}
+
+console.log(isBalanced('[()][{}[{}[{}]]][]{}[]{}[]{{}({}(){({{}{}[([[]][[]])()]})({}{{}})})}'))
