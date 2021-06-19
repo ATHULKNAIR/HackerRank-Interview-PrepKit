ec365f91e99d47180848b10d9aebb4227738e689
ATHULKNAIR <athul2639@gmail.com>
2021-06-19 21:47:00 +0530

    Bubble Sort in JS

---
+++ Sorting-Challenges/BubbleSort.js
@@ -0,0 +1,16 @@
+function countSwaps(a) {
+    var swaps = 0;
+    for(let i = 0; i<a.length;i++){
+        for(let j = 0;j<a.length-1;j++){
+            if(a[j]>a[j+1]){
+                var temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+                swaps++
+            }
+        }
+    }
+     console.log('Array is sorted in ' + swaps + ' swaps.')
+     console.log('First Element: ' + a[0])
+     console.log('Last Element: ' + a[a.length - 1])
+ }
