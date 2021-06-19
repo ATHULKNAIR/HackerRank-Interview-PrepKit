30a3f728856db0351e7e615528db086ca04abc47
ATHULKNAIR <athul2639@gmail.com>
2021-06-19 21:47:45 +0530

    Largest Rectangle

--- Stack_Queues-Challenges/LargestRectangle.js
+++ Stack_Queues-Challenges/LargestRectangle.js
@@ -6,23 +6,14 @@ function largestRectangle(h) {
     let stack = []
     
     while (i < heights.length) {
-        console.log("Value of i : "+i)
-        console.log("Length of heights array : "+heights.length)
         if (!stack.length || heights[stack[stack.length - 1]] <= heights[i]) {
             stack.push(i++)
-            console.log(stack)
-            console.log("after pushing to stack , Value of i : "+i)
         } else {
             const top = stack.pop()
-            console.log("Top is : "+top)
             const lastPosition = stack[stack.length - 1]
-            console.log("LastPosition : "+lastPosition)
             const buildingHeight = heights[top]
-            console.log("BuildingHeight : "+buildingHeight)
             const numberOfBuildings = stack.length ? (i - lastPosition - 1) : i
-            console.log("No.Of Buildings : "+ numberOfBuildings)
             area = Math.max(area, buildingHeight * numberOfBuildings)
-            console.log(area)
         }
     }
     return area
