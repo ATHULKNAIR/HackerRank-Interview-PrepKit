b9a4acf415ac52a8fd04073cb775e1402a021ecc
ATHULKNAIR <athul2639@gmail.com>
2021-07-15 16:54:47 +0530

    Reverse string

--- MyQuestions/ReverseString.js
+++ MyQuestions/ReverseString.js
@@ -1,21 +1,22 @@
-let x = "i love Javascript"   
+let x = "i love Javascript"
 
 //  print "tpircsavaJ evol i"
- 
+
 //  1)
 
-    let y = ""
+let y = ""
 
-    for(let i = x.length-1;i>=0;i--){
-        y+=x[i]
-    }
+for (let i = x.length - 1; i >= 0; i--) {
+    y += x[i]
+}
 
-    console.log(y)
+console.log(y)
+
 
 //  2)
 
-    let z = x.split('').reverse().join('')
-    console.log(z)
+let z = x.split('').reverse().join('')
+console.log(z)
 
 /////////////////////////////////////////////
 
@@ -35,13 +36,18 @@ console.log(rev)
 let reverseStr = x.split(' ')
 let b = ""
 let c = ""
-for(let i = 0; i <=reverseStr.length-1;i++){
+for (let i = 0; i <= reverseStr.length - 1; i++) {
 
-    for(let j = reverseStr[i].length ;j>=0;j--){
-     c =   reverseStr[i].split('').reverse().join('')
-   
+    for (let j = reverseStr[i].length; j >= 0; j--) {
+        c = reverseStr[i].split('').reverse().join('')
+
     }
-  b =b+c+" "
- 
+    b = b + c + " "
+
 }
 console.log(b)
+
+
+
+
+
