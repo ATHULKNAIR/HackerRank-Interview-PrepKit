09fd780358409ccd3d966ad189a5e7be0832ad29
ATHULKNAIR <77236185+ATHULKNAIR@users.noreply.github.com>
2021-08-06 23:59:57 +0530

    2d Array

--- Array-Challenges/2D-Array.js
+++ Array-Challenges/2D-Array.js
@@ -6,7 +6,6 @@ function hourglassSum(arr) {
         for (let j = 0; j <= 3; j++) {
             let sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1]
                 + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
-
             if (sum > count) {
                 count = sum
             }
