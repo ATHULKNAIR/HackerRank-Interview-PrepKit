b0a19d6b1202c0b122a1246f7b5404ca0eb751a6
ATHULKNAIR <77236185+ATHULKNAIR@users.noreply.github.com>
2021-08-21 22:37:26 +0530

    Bubble sort algorithm

--- Sorting-Challenges/BubbleSort.js
+++ Sorting-Challenges/BubbleSort.js
@@ -10,7 +10,6 @@ function countSwaps(a) {
             }
         }
     }
-    
      console.log('Array is sorted in ' + swaps + ' swaps.')
      console.log('First Element: ' + a[0])
      console.log('Last Element: ' + a[a.length - 1])
